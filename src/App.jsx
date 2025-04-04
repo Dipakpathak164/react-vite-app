06e92575f3dad0b1dcfa892ea44a3d33972b3f90
Dipak Pathak <dipak@techasoft.com>
2025-04-04 11:22:55 +0530

    Initial React Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello, GitHub Pages!</h1>
+      <p>This is a simple React Vite app deployed on GitHub Pages.</p>
+    </div>
+  );
+}
+
+export default App;
